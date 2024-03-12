8064e152f0bb0d0f223da3c122a9cf16bf1eb2b1
Bharti <pratham.bharti@unisys.com>
2024-03-12 11:48:37 +0530

    first commit

---
+++ public/main.js
@@ -0,0 +1,53 @@
+const socket=io()
+
+const clientsToatal=document.getElementById('clients-total');
+const messageContainer=document.getElementById('message-container')
+const nameInput=document.getElementById('name-input')
+const messageForm =document.getElementById('message-form')
+const messageInput=document.getElementById('message-input')
+
+
+messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    sendMessage();
+    
+})
+
+socket.on('clients-total',(data)=>{
+    clientsToatal.innerHTML= `Total Clients: ${data}`
+})
+
+const sendMessage=()=>{
+    if(messageInput.value=="") return
+    console.log(messageInput.value)
+    const data={
+        name:nameInput.value,
+        message:messageInput.value,
+        dateTime: new Date()
+    }
+
+    socket.emit('message',data);
+    addMessage(true,data)
+    messageInput.value="";
+}
+
+socket.on('chat-message',(data)=>{
+    console.log(data)
+    addMessage(false,data);
+})
+
+const addMessage=(iaOwnMessage,data)=>{
+    const element=`
+    <li class="${iaOwnMessage?"message-right":"message-left"}">
+          <p class="message">
+            ${data.message}
+            <span>bluebird - ${moment(data.dateTime).fromNow()}</span>
+          </p>
+        </li>`
+    messageContainer.innerHTML+=element;
+    scrollToBottom();
+}
+
+const scrollToBottom=()=>{
+    messageContainer.scrollTo(0,messageContainer.scrollHeight);
+}
